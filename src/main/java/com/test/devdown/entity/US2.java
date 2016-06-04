be11f01090f86043ea8e46411fa606b78743135c
peter <peter612git@gmail.com>
2016-06-04 23:47:43 +0200

    US2

---
+++ src/main/java/com/test/devdown/entity/US2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.devdown.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Peter
+ */
+
+@Entity
+@Table(name = "US2")
+public class US2 implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+}
