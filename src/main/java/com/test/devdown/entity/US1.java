0b22e3b0d575a930fc32b4c38133bd927258ce98
peter <peter612git@gmail.com>
2016-06-04 23:35:57 +0200

    US1

---
+++ src/main/java/com/test/devdown/entity/US1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.devdown.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Peter
+ */
+
+@Entity
+@Table(name = "US1")
+public class US1 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    
+}
